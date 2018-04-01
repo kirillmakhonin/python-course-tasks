e2d838a2072932a04dd5869957fbf1665fd79fa8
Ivan Nemenchinskii <nemenivan@gmail.com>
2018-04-02 00:38:46 +0300

    fix2 task_L2.1

--- tasks/task_2.1_intersect_union/intersect_union.py
+++ tasks/task_2.1_intersect_union/intersect_union.py
@@ -1,7 +1,6 @@
 from functools import reduce
 from operator import contains
-test = [[y for y in range(1,x)] for x in range(4,100)]
-intersect_func = lambda *args: [item for item in args[0] if map(lambda collection: item in collection, args[1:])]
+#test = [[y for y in range(1,x)] for x in range(4,100)]
     
 def intersect(*args):
     '''Вычисляет пересечение множеств удалением невалидных элементов из первого множества '''
