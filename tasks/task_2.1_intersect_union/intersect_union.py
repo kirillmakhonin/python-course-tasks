000fa247f8966de7f6b05a0a5677cf88e4417585
Ivan Nemenchinskii <nemenivan@gmail.com>
2018-04-02 00:37:10 +0300

    fix task_L2.1

--- tasks/task_2.1_intersect_union/intersect_union.py
+++ tasks/task_2.1_intersect_union/intersect_union.py
@@ -1,10 +1,26 @@
-def code(a: str) -> int:
-  result = 0
-  for char in a:
-    o = ord(char)
-    while o>0:
-      result *= 10
-      o = o // 10
-    result += ord(char)
-  return result
+from functools import reduce
+from operator import contains
+test = [[y for y in range(1,x)] for x in range(4,100)]
+intersect_func = lambda *args: [item for item in args[0] if map(lambda collection: item in collection, args[1:])]
+    
+def intersect(*args):
+    '''Вычисляет пересечение множеств удалением невалидных элементов из первого множества '''
+    result = list(args[0])
+    for collection in args[1:]:
+        for item in result[:]:
+            if item not in collection:
+                result.pop(item)
+    return result
 
+
+def union(*args):
+    result = list()
+    for collection in args:
+        for item in collection:
+            if item not in result:
+                result.append(item)
+    return sorted(result)
+
+
+# intersect в функциональном виде
+intersect_func = lambda *args: [item for item in args[0] if map(lambda collection: item in collection, args[1:])]
