ca86c0296b9e911abb2cffdb3b48aec6fb41f263
Ivan Nemenchinskii <nemenivan@gmail.com>
2018-05-08 16:46:15 +0300

    wip

--- tasks/task_11.2_triangle/gerone.py
+++ tasks/task_11.2_triangle/gerone.py
@@ -1,15 +1,71 @@
 #!/usr/bin/env python
 """
-Module computes triangle area from 3 point coordinates
-using Gerones' formula
+Provides triangle class and functions for computing
+triangle area using Gerones formula
 """
 
 
-class NotATriangle(Exception):
+class TriangleError(Exception):
     """
-    Exception for invalid triangles
+    Exception for all things with triangles
     """
     pass
+
+
+class Triangle():
+    """
+    Triangle class stores vertices coordinates and computes area
+
+    >>> Triangle([(0, 0), (0, 3), (4, 0)]).area
+    6.0
+    """
+    def __init__(self, points):
+        """
+        Initiate triangle with points coordinates
+
+        :param points: vertices coordinates [(1, 2), (3, 4), (0,5)]
+        :type points: list(tuple)
+        """
+        self.points = points
+
+    def __setattr__(self, name, value):
+        # Perform checks on setting instance.points attribute
+        if name == 'points':
+            self.points_valid(value) # raises TriangleError 
+        super().__setattr__(name, value)
+
+    @property
+    def area(self):
+        """
+        Compute triangle area
+
+        :return: Triangle area
+        :rtype: float
+        """
+        return gerone_from_coords(*self.points)
+
+    @staticmethod
+    def points_valid(points):
+        """
+        Perform checks to verify triangle from points has some area,
+        raises TriangleError
+
+        :param points: vertices coordinates [(1, 2), (3, 4), (0,5)]
+        :type points: list(tuple)
+        :return: triangle has valid coordinates
+        :rtype: bool
+        :raises TriangleError: if somehow triangle points are in conflict with basic math
+        """
+        a = distance(points[0], points[1])
+        b = distance(points[0], points[2])
+        c = distance(points[1], points[2])
+        if a*b*c == 0:
+            raise TriangleError('Some points are in the same place')
+        elif (a == b+c or b == a+c or c == a+b):
+            raise TriangleError('Points on the same line')
+        elif (a > b+c or b > a+c or c > a+b):
+            raise TriangleError('One side is longer than sum of other two')
+        return True
 
 
 def gerone_from_coords(point1, point2, point3):
@@ -22,27 +78,28 @@ def gerone_from_coords(point1, point2, point3):
     :type point2: tuple(float, float)
     :param point3: Third point
     :type point3: tuple(float, float)
-    :return: float -- triangle area
+    :return: triangle area
+    :rtype: float
 
     >>> gerone_from_coords((0,0), (0,3), (4,0))
-    5.0
+    6.0
     """
     a = side_len(*point1, *point2)
     b = side_len(*point1, *point3)
     c = side_len(*point2, *point3)
 
     if a*b*c == 0:
-        raise NotATriangle('Some points are the same')
+        raise NotATriangle('Some points are in the same place')
     elif (a == b+c or b == a+c or c == a+b):
         raise NotATriangle('Points on the same line')
     elif (a > b+c or b > a+c or c > a+b):
-        raise NotATriangle
+        raise NotATriangle('One side is longer than sum of other two')
     return not_safe_gerone(a, b, c)
 
 
 def not_safe_gerone(a, b, c):
     """
-    Compute triangle area from sides' lengths
+    Compute triangle area from valid sides lengths
 
     :param float a: first side length
     :param float b: second side length
@@ -56,20 +113,21 @@ def not_safe_gerone(a, b, c):
     return (p*(p-a)*(p-b)*(p-c))**(1/2)
 
 
-def side_len(x1, y1, x2, y2):
+def distance(point1, point2):
     """
-    Side length from point coordinates
+    Distance between two points
 
+    :param tuple point1: first point coordinates (1, 2)
+    :param tuple point2: second point coordinates (3.0, 4)
+    :return: line segment length
+    :rtype: float
 
-    :param float x1: first point x-axis coordinate
-    :param float y1: first point y-axis coordinate
-    :param float x2: second point x-axis coordinate
-    :param float y2: second point y-axis coordinate
-    :return: float -- line length
-
-    >>> side_len(0,0,3,4.0)
+    >>> distance((0, 0) (3, 4.0))
     5.0
     """
+    x1, y1 = point1
+    x2, y2 = point2
+    
     return ((x1-x2)**2+(y1-y2)**2)**(1/2)
 
 
@@ -77,22 +135,26 @@ def point_input(num):
     """
     Input two coordinates
 
-    :return: (float, float) -- coodinates
-    """
+    :param int num: points number to print
+    :return: point coordinates (x, y)
+    :rtype: tuple
 
-    x = float(input(f'{num} point x: '))
-    y = float(input(f'        y: '))
-    return (x, y)
+    doctests work poorly with promts
+    """
+    while True:
+        try:
+            x = float(input(f'{num} point x: '))
+            y = float(input(f'{" "*7+" "*len(str(num))}y: '))
+            break
+        except ValueError:
+            print('Invalid coodinate!')
+    return x, y
 
 
 if __name__ == '__main__':
     while True:
         # Input block
-        try:
-            coords = [point_input(i) for i in range(1, 4)]
-        except ValueError:
-            print('Invalid coodinate!')
-            continue
+        coords = [point_input(i) for i in range(1, 4)]
 
         # Calculation block
         try:
--- tasks/task_11.2_triangle/test_gerone.py
+++ tasks/task_11.2_triangle/test_gerone.py
@@ -1,20 +1,43 @@
-import sys
-import unittest
+#!/usr/bin/env python
 from unittest.mock import patch
-import io
-mocked_stdin = """\
-0
-0
-0
-3
-4
-0
-"""
+import unittest
+import gerone
 
-#@patch('builtins.input', side_effect=mocked_stdin.split())
-@patch(target='builtins.input', side_effect=lambda : '0')
+
+mocked_input = """0 0 0 3 4 0""".split()
+
+
+@patch(target='builtins.input', side_effect=mocked_input)
 class TestInput(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
     
-    def test_valid_input(self, *args):
-        print(args)
+    def test_valid_input(self, mock):
         self.assertEqual(input(), '0')
+
+    def test_something(*args):
+        pass
+
+
+class TestTriangleClass(unittest.TestCase):
+    valid_points = [
+            (0, 0),
+            (3, 0),
+            (0, 4)]
+    points_on_same_line = [
+            (0, 0),
+            (0, 5),
+            (0, 6)]
+    
+    def test_invalid_points_init(self):
+        with self.assertRaises(gerone.TriangleError):
+            gerone.Triangle(self.points_on_same_line)
+    def test_valid_points_init(self):
+        gerone.Triangle(self.valid_points)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+    doctest.testmod(gerone)
